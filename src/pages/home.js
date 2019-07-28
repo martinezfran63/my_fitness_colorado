e4c42a2f4c36a5fa2b26da6fb5ab7aecd7b1170a
Francisco <fmartinez@weedmaps.com>
2019-07-28 15:30:13 -0600

    add random text to grid one

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,25 @@
 import React from "react";
-import logo from "../logo.svg";
-import { Grid } from "@material-ui/core";
+import logo from "../rocky_mountains.jpg";
+import { Grid, Typography } from "@material-ui/core";
 
 class Home extends React.Component {
   render() {
     return (
       <Grid container>
         <Grid item sm>
-          Home page!
+        <Typography variant="h2" gutterBottom>
+        Find support and encouragement from the most positive community on the web.
+      </Typography>
+
+      <Typography variant="h6" gutterBottom>
+      Our community forum is a great place to share helpful workout and nutrition information and cheer each other on. Share tips, challenges and triumphs, results and progress with Fitness Blender members from all over the world.
+      </Typography>
+
+      <Typography variant="subtitle2" gutterBottom>
+        Learn more here
+      </Typography>
+
+
         </Grid>
         <Grid item sm>
 
