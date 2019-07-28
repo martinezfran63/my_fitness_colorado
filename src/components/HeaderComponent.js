46d84011ce0f545944739ca4bd04e560cdf84b4e
Francisco <fmartinez@weedmaps.com>
2019-07-28 16:40:28 -0600

    fix logo & use styled component

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -2,15 +2,23 @@ import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import { AppBar, Toolbar } from  "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
-import logo from "../images/MyFitnessColorado_AppBar.jpg";
+import logo from "../images/MyFitnessColoradoLogo.jpg";
+import styled from 'styled-components';
+
+const HeaderImg = styled.div`
+  background-image: url(${logo});
+  width: 45px;
+  height: 45px;
+  background-repeat: no-repeat;
+`;
 
 function HeaderComponent() {
   return (
     <Fragment>
       <AppBar position="static" style={{ background: '#ffffff' }}>
         <Toolbar>
-		  <img src={logo} className="App-logo" alt="logo" />
-          <Typography variant="headline" color="inherit">
+          <HeaderImg></HeaderImg>
+          <Typography >
             My Fitness Colorado
           </Typography>
 		  <ul>
