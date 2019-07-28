75f143bc66d692d06e88d6ee14fbd13d9011949e
Francisco <fmartinez@weedmaps.com>
2019-07-28 14:22:16 -0600

    add logo to appbar

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import logo from "../logo.svg";
 
 function HeaderComponent() {
   return (
     <Fragment>
       <AppBar position="static">
         <Toolbar>
+		  <img src={logo} className="App-logo" alt="logo" />
           <Typography variant="headline" color="inherit">
             My Fitness Colorado
           </Typography>
-        </Toolbar>
-      </AppBar>
-      <ul>
+		  <ul>
         <li>
           <Link to="/"> Home </Link>
         </li>
@@ -28,6 +28,9 @@ function HeaderComponent() {
           <Link to="/contact"> Contact </Link>
         </li>
       </ul>
+        </Toolbar>
+      </AppBar>
+      
     </Fragment>
   );
 }
